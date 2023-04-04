c72bb0d674423cd0bba23dcd259ea8d079112749
Flavio Gezzi <gezzi.1958690@studenti.uniroma1.it>
2023-04-04 13:23:03 +0200

    add starspace

--- main.js
+++
@@ -1,3 +0,0 @@
-import * as THREE from 'three';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
