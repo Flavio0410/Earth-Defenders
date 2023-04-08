2863e31b83754e64941812ca8cff79737c03f09c
Flavio Gezzi <gezzi.1958690@studenti.uniroma1.it>
2023-04-08 18:46:16 +0200

    try to add cubo at center scene

--- src/starspace.js
+++ src/starspace.js
@@ -1,68 +1,78 @@
-
-let scene, camera, renderer, stars, starGeo;
+let scene, camera, renderer, stars, starGeo, model;
 
 function init() {
-
     scene = new THREE.Scene();
     scene.background = new THREE.Color(0x000000);
 
-    camera = new THREE.PerspectiveCamera(60,window.innerWidth / window.innerHeight, 0.1, 5000);
-    camera.position.z = 0;
-    camera.rotation.x = Math.PI/2;
-    //camera.rotation.x = 1.16;
+    camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 0.1, 1000);
+    camera.position.z = 5;
+    camera.rotation.x = 0.1;
 
-    renderer = new THREE.WebGLRenderer();
+    renderer = new THREE.WebGLRenderer({ antialias: true });
     renderer.setSize(window.innerWidth, window.innerHeight);
     document.body.appendChild(renderer.domElement);
 
+
     starGeo = new THREE.Geometry();
-    for(let i=0;i<6000;i++) {
-    star = new THREE.Vector3(
-        Math.random() * 600 - 300,
-        Math.random() * 600 - 300,
-        Math.random() * 600 - 300
-    );
-    star.velocity = 0;
-    star.acceleration = 0.001;
-    starGeo.vertices.push(star);
+    for (let i = 0; i < 6000; i++) {
+        star = new THREE.Vector3(
+            Math.random() * 600 - 300,
+            Math.random() * 600 - 300,
+            Math.random() * 600 - 300
+        );
+        star.velocity = 0;
+        star.acceleration = 0.001;
+        starGeo.vertices.push(star);
     }
 
-    let sprite = new THREE.TextureLoader().load('public/assets/star.png');
+    let sprite = new THREE.TextureLoader().load('../public/assets/star.png');
     let starMaterial = new THREE.PointsMaterial({
-    color: 0xaaaaaa,
-    size: 0.6,
-    map: sprite
+        color: 0xaaaaaa,
+        size: 0.6,
+        map: sprite
     });
 
-    stars = new THREE.Points(starGeo,starMaterial);
+    stars = new THREE.Points(starGeo, starMaterial);
     scene.add(stars);
+
+    const geometry = new THREE.BoxGeometry( 1.5, 1.5, 1.5 );
+    const material = new THREE.MeshBasicMaterial( { color: 0xffca27 } );
+    const cube = new THREE.Mesh( geometry, material );
+    cube.position.set(2, 2, 0);
+    scene.add( cube );
+
+
 
     window.addEventListener("resize", onWindowResize, false);
 
     animate();
-}            
-
-
+}
 
 function onWindowResize() {
     camera.aspect = window.innerWidth / window.innerHeight;
     camera.updateProjectionMatrix();
     renderer.setSize(window.innerWidth, window.innerHeight);
-    }
+}
+
 function animate() {
     starGeo.vertices.forEach(p => {
-    p.velocity += p.acceleration
-    p.y -= p.velocity;
-    
-    if (p.y < -200) {
-        p.y = 200;
-        p.velocity = 0;
-    }
+        p.velocity += p.acceleration
+        p.y -= p.velocity;
+
+        if (p.y < -200) {
+            p.y = 200;
+            p.velocity = 0;
+        }
     });
     starGeo.verticesNeedUpdate = true;
-    stars.rotation.y +=0.003;
+    stars.rotation.y += 0.003;
 
+
+    if (model) {
+        model.rotation.y += 0.01;
+    }
     renderer.render(scene, camera);
     requestAnimationFrame(animate);
 }
+
 init();
