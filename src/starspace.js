6f99819e849cfc26ab5931b248a6d7e0b0290d49
Flavio Gezzi <gezzi.1958690@studenti.uniroma1.it>
2023-04-11 12:28:20 +0200

    convert spaceship.js in class

--- src/starspace.js
+++ src/starspace.js
@@ -1,78 +1,78 @@
-let scene, camera, renderer, stars, starGeo, model;
+class MyScene {
+    constructor() {
+        this.scene = new THREE.Scene();
+        this.scene.background = new THREE.Color(0x000000);
 
-function init() {
-    scene = new THREE.Scene();
-    scene.background = new THREE.Color(0x000000);
+        this.camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 0.1, 1000);
+        this.camera.position.z = 5;
+        this.camera.rotation.x = 0.1;
 
-    camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 0.1, 1000);
-    camera.position.z = 5;
-    camera.rotation.x = 0.1;
+        this.renderer = new THREE.WebGLRenderer({ antialias: true });
+        this.renderer.setSize(window.innerWidth, window.innerHeight);
+        document.body.appendChild(this.renderer.domElement);
 
-    renderer = new THREE.WebGLRenderer({ antialias: true });
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    document.body.appendChild(renderer.domElement);
-
-
-    starGeo = new THREE.Geometry();
-    for (let i = 0; i < 6000; i++) {
-        star = new THREE.Vector3(
-            Math.random() * 600 - 300,
-            Math.random() * 600 - 300,
-            Math.random() * 600 - 300
-        );
+        this.starGeo = new THREE.Geometry();
+        for (let i = 0; i < 6000; i++) {
+            let star = new THREE.Vector3(
+                Math.random() * 600 - 300,
+                Math.random() * 600 - 300,
+                Math.random() * 600 - 300
+                );
         star.velocity = 0;
         star.acceleration = 0.001;
-        starGeo.vertices.push(star);
+        this.starGeo.vertices.push(star);
+        }
+
+        let sprite = new THREE.TextureLoader().load('../public/assets/star.png');
+        let starMaterial = new THREE.PointsMaterial({
+            color: 0xaaaaaa,
+            size: 0.6,
+            map: sprite
+            });
+
+        this.stars = new THREE.Points(this.starGeo, starMaterial);
+        this.scene.add(this.stars);
+
+        const geometry = new THREE.BoxGeometry(1.5, 1.5, 1.5);
+        const material = new THREE.MeshBasicMaterial({ color: 0xffca27 });
+        this.cube = new THREE.Mesh(geometry, material);
+        this.cube.position.set(2, 2, 0);
+        this.scene.add(this.cube);
+
+        window.addEventListener("resize", this.onWindowResize.bind(this), false);
     }
 
-    let sprite = new THREE.TextureLoader().load('../public/assets/star.png');
-    let starMaterial = new THREE.PointsMaterial({
-        color: 0xaaaaaa,
-        size: 0.6,
-        map: sprite
-    });
 
-    stars = new THREE.Points(starGeo, starMaterial);
-    scene.add(stars);
-
-    const geometry = new THREE.BoxGeometry( 1.5, 1.5, 1.5 );
-    const material = new THREE.MeshBasicMaterial( { color: 0xffca27 } );
-    const cube = new THREE.Mesh( geometry, material );
-    cube.position.set(2, 2, 0);
-    scene.add( cube );
-
-
-
-    window.addEventListener("resize", onWindowResize, false);
-
-    animate();
-}
-
-function onWindowResize() {
-    camera.aspect = window.innerWidth / window.innerHeight;
-    camera.updateProjectionMatrix();
-    renderer.setSize(window.innerWidth, window.innerHeight);
-}
-
-function animate() {
-    starGeo.vertices.forEach(p => {
+    onWindowResize() {
+      this.camera.aspect = window.innerWidth / window.innerHeight;
+      this.camera.updateProjectionMatrix();
+      this.renderer.setSize(window.innerWidth, window.innerHeight);
+    }
+  
+    animate() {
+      this.starGeo.vertices.forEach(p => {
         p.velocity += p.acceleration
         p.y -= p.velocity;
-
+  
         if (p.y < -200) {
-            p.y = 200;
-            p.velocity = 0;
+          p.y = 200;
+          p.velocity = 0;
         }
-    });
-    starGeo.verticesNeedUpdate = true;
-    stars.rotation.y += 0.003;
-
-
-    if (model) {
-        model.rotation.y += 0.01;
+      });
+      this.starGeo.verticesNeedUpdate = true;
+      this.stars.rotation.y += 0.003;
+  
+      if (this.model) {
+        this.model.rotation.y += 0.01;
+      }
+      this.renderer.render(this.scene, this.camera);
+      requestAnimationFrame(this.animate.bind(this));
     }
-    renderer.render(scene, camera);
-    requestAnimationFrame(animate);
-}
-
-init();
+  
+    init() {
+      this.animate();
+    }
+  }
+  
+  const myScene = new MyScene();
+  myScene.init();
