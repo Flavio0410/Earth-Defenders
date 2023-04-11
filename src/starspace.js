e570e69441a095bc9c25f1ff046ffb110950888a
Flavio Gezzi <gezzi.1958690@studenti.uniroma1.it>
2023-04-11 18:25:04 +0200

    add browser icon

--- src/starspace.js
+++ src/starspace.js
@@ -1,69 +1,77 @@
-//Import the THREE.js library
+// Importa la libreria THREE.js
 import * as THREE from "https://cdn.skypack.dev/three@0.117.0/build/three.module.js";
-// To allow for the camera to move around the scene
+// Per consentire alla telecamera di muoversi intorno alla scena
 import { OrbitControls } from "https://cdn.skypack.dev/three@0.117.0/examples/jsm/controls/OrbitControls.js";
-// To allow for importing the .gltf file
+// Per consentire l'importazione del file .gltf
 import { GLTFLoader } from "https://cdn.skypack.dev/three@0.117.0/examples/jsm/loaders/GLTFLoader.js";
 
-let scene, camera, renderer, stars, starGeo, star;
+class starspace {
+  constructor() {
+    // Inizializza la scena, la telecamera e il renderer
+    this.scene = new THREE.Scene();
+    this.camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 1, 1000);
+    this.camera.position.z = 1;
+    this.camera.rotation.x = Math.PI/2;
+    this.renderer = new THREE.WebGLRenderer();
+    this.renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(this.renderer.domElement);
 
-function init() {
-
-  scene = new THREE.Scene();
-
-  camera = new THREE.PerspectiveCamera(60,window.innerWidth / window.innerHeight, 1, 1000);
-  camera.position.z = 1;
-  camera.rotation.x = Math.PI/2;
-
-  renderer = new THREE.WebGLRenderer();
-  renderer.setSize(window.innerWidth, window.innerHeight);
-  document.body.appendChild(renderer.domElement);
-
-  starGeo = new THREE.Geometry();
-  for(let i=0;i<6000;i++) {
-    star = new THREE.Vector3(
-      Math.random() * 600 - 300,
-      Math.random() * 600 - 300,
-      Math.random() * 600 - 300
-    );
-    star.velocity = 0;
-    star.acceleration = 0.02;
-    starGeo.vertices.push(star);
-  }
-
-  let sprite = new THREE.TextureLoader().load( '../public/assets/star.png' );
-  let starMaterial = new THREE.PointsMaterial({
-    color: 0xaaaaaa,
-    size: 0.7,
-    map: sprite
-  });
-
-  stars = new THREE.Points(starGeo,starMaterial);
-  scene.add(stars);
-
-  window.addEventListener("resize", onWindowResize, false);
-
-  animate(); 
-}
-function onWindowResize() {
-    camera.aspect = window.innerWidth / window.innerHeight;
-    camera.updateProjectionMatrix();
-    renderer.setSize(window.innerWidth, window.innerHeight);
-  }
-function animate() {
-  starGeo.vertices.forEach(p => {
-    p.velocity += p.acceleration
-    p.y -= p.velocity;
-    
-    if (p.y < -200) {
-      p.y = 200;
-      p.velocity = 0;
+    // Crea una geometria per le stelle
+    this.starGeo = new THREE.Geometry();
+    for(let i=0; i<6000; i++) {
+      let star = new THREE.Vector3(
+        Math.random() * 600 - 300,
+        Math.random() * 600 - 300,
+        Math.random() * 600 - 300
+      );
+      star.velocity = 0;
+      star.acceleration = 0.02;
+      this.starGeo.vertices.push(star);
     }
-  });
-  starGeo.verticesNeedUpdate = true;
-  stars.rotation.y +=0.002;
 
-  renderer.render(scene, camera);
-  requestAnimationFrame(animate);
+    // Crea un materiale per le stelle
+    let sprite = new THREE.TextureLoader().load('../public/assets/star.png');
+    let starMaterial = new THREE.PointsMaterial({
+      color: 0xaaaaaa,
+      size: 0.7,
+      map: sprite
+    });
+
+    // Crea un oggetto Points per le stelle e lo aggiunge alla scena
+    this.stars = new THREE.Points(this.starGeo, starMaterial);
+    this.scene.add(this.stars);
+
+    // Aggiunge un listener per la ridimensione della finestra
+    window.addEventListener("resize", this.onWindowResize.bind(this), false);
+
+    // Avvia l'animazione
+    this.animate();
+  }
+
+  // Funzione chiamata quando la finestra viene ridimensionata
+  onWindowResize() {
+    this.camera.aspect = window.innerWidth / window.innerHeight;
+    this.camera.updateProjectionMatrix();
+    this.renderer.setSize(window.innerWidth, window.innerHeight);
+  }
+
+  // Funzione per l'animazione delle stelle
+  animate() {
+    this.starGeo.vertices.forEach(p => {
+      p.velocity += p.acceleration;
+      p.y -= p.velocity;
+      if (p.y < -200) {
+        p.y = 200;
+        p.velocity = 0;
+      }
+    });
+    this.starGeo.verticesNeedUpdate = true;
+    this.stars.rotation.y += 0.002;
+
+    this.renderer.render(this.scene, this.camera);
+    requestAnimationFrame(this.animate.bind(this));
+  }
 }
-init();
+
+// Crea un'istanza della classe StarField e inizia l'animazione delle stelle
+let start = new starspace();
