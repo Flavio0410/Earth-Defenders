16bbadf863f209645a10bd276ae63378239359b2
Flavio Gezzi <gezzi.1958690@studenti.uniroma1.it>
2023-05-03 12:55:31 +0200

    add earth rotation at hover button

--- src/EarthLoader.js
+++ src/EarthLoader.js
@@ -43,36 +43,48 @@ class EarthLoader
 
     const ambientLight = new THREE.AmbientLight(0xffffff, 0.7); //crea una nuova luce ambientale bianca con una intensità del 70%
     this.scene.add(ambientLight); //aggiunge la luce alla scena Three.js
-    
-    // window.addEventListener("resize", () => this.onWindowResize()); //aggiunge un listener per l'evento "resize" della finestra del browser che richiama la funzione onWindowResize()
+  
     
     this.container.appendChild(this.renderer.domElement); //aggiunge il canvas della scena Three.js al container HTML della pagina web
     
-    this.animate(); //avvia la funzione di animazione della scena Three.js
+    this.rotate(); //avvia la funzione di animazione della scena Three.js
 
+    document.getElementsByClassName("playbutton")[0].addEventListener("mouseover", () => this.rotatefast()); //aggiunge un listener per l'evento "click" del bottone play che richiama la funzione play()
+
+    document.getElementsByClassName("playbutton")[0].addEventListener("mouseout", () => this.rotatenormal()); //aggiunge un listener per l'evento "click" del bottone play che richiama la funzione play()
 
     
   }
 
 
-//   onWindowResize() { //definisce una funzione onWindowResize()
-//     this.camera.aspect = containerId.innerWidth / containerId.innerHeight; //aggiorna il rapporto di aspetto della telecamera in base alla larghezza e all'altezza della finestra del browser
-//     this.camera.updateProjectionMatrix(); //aggiorna la matrice di proiezione della telecamera
-//     this.renderer.setSize(containerId.innerWidth, containerId.innerHeight); //imposta la nuova dimensione del canvas del renderer in base alla larghezza e all'altezza della finestra del browser
-// }
-
-
-animate() { //definisce una funzione animate()
-    requestAnimationFrame(() => this.animate()); //richiede l'animazione della funzione animate() usando requestAnimationFrame(), che viene richiamata ad ogni frame di animazione
+rotate() { //definisce una funzione rotate che avvia l'animazione della terra attorno al proprio asse
+    requestAnimationFrame(() => this.rotate()); //richiede l'animazione della funzione animate() usando requestAnimationFrame(), che viene richiamata ad ogni frame di animazione
     if (this.object) { //verifica se l'oggetto 3D è stato caricato
       this.object.rotation.y += 0.0015; //aggiorna la rotazione dell'oggetto 3D lungo l'asse Y
-      //this.object.rotation.z += 0.0010; //aggiorna la rotazione dell'oggetto 3D lungo l'asse Z
-  
     }
     this.renderer.render(this.scene, this.camera); //renderizza la scena Three.js utilizzando il renderer
   }
 
+rotatefast(){
+  requestAnimationFrame(() => this.rotatefast()); //richiede l'animazione della funzione animate() usando requestAnimationFrame(), che viene richiamata ad ogni frame di animazione
+  if (this.object) { //verifica se l'oggetto 3D è stato caricato
+    this.object.rotation.y += 0.1; //aggiorna la rotazione dell'oggetto 3D lungo l'asse Y
+  }
+  this.renderer.render(this.scene, this.camera); //renderizza la scena Three.js utilizzando il renderer
+  console.log("rotate");
+}
+
+rotatenormal(){
+  requestAnimationFrame(() => this.rotatenormal()); //richiede l'animazione della funzione animate() usando requestAnimationFrame(), che viene richiamata ad ogni frame di animazione
+  if (this.object) { //verifica se l'oggetto 3D è stato caricato
+    this.object.rotation.y-=0.1; //aggiorna la rotazione dell'oggetto 3D lungo l'asse Y
+    this.object.rotation.y += 0.0015; //aggiorna la rotazione dell'oggetto 3D lungo l'asse Y
+  }
+  this.renderer.render(this.scene, this.camera); //renderizza la scena Three.js utilizzando il renderer
+}
+
+
 
 }
 
-const tt = new EarthLoader("container3D", "../public/assets/models/earth/scene.gltf");
+const tt = new EarthLoader("EarthContainer", "../public/assets/models/earth/scene.gltf");
