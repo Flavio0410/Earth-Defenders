7bc2bceadd32917708869122152ec324a638c856
Flavio Gezzi <gezzi.1958690@studenti.uniroma1.it>
2023-05-13 15:23:46 +0200

    fix form validation

--- src/formValidation.js
+++ src/formValidation.js
@@ -29,32 +29,52 @@ $(function(){
     $("#registerForm").validate({
         rules:{
             usernamesignup:{
+                required:true
+            },
+
+            namesignup:{
                 required:true,
             },
-            passwordsignup:{
+
+            surnamesignup:{
                 required:true,
-                rangelength: [8, 20]
             },
-            confirmpasswordsignup:{
-                required:true,
-                equalTo: "#passwordsignup"
-            },
+
             emailsignup:{
                 required:true,
                 email:true
             },
-            namesignup:{
+
+            passwordsignup:{
                 required:true,
+                rangelength: [8, 20]
             },
-            surnamesignup:{
+
+            confirmpasswordsignup:{
                 required:true,
+                equalTo: "#passwordsignup"
             },
+
         },
         messages:{
-            username:{
-                required:"Inserisci il tuo username",
-
+            usernamesignup:{
+                required: "Inserisci il tuo username",
             },
+
+
+            namesignup:{
+                required: "Inserisci il tuo nome",
+            },
+
+            surnamesignup:{
+                required: "Inserisci il tuo cognome",
+            },
+
+            emailsignup:{
+                required: "Inserisci la tua email",
+                email: "Inserisci una email valida"
+            },
+
             passwordsignup:{
                 required: "Inserisci la tua password",
                 rangelength: "La password deve essere lunga minimo 8 e massimo 20 caratteri"
@@ -63,10 +83,7 @@ $(function(){
                 required: "Inserisci la tua password",
                 equalTo: "Le due password non coincidono"
             },
-            emailsignup:{
-                required: "Inserisci la tua email",
-                email: "Inserisci una email valida"
-            }
+
         },
         submitHandler:function(form){
             form.submit();
