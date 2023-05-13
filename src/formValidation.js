45ba9ddf8b2683cbd339cccb8982dafcd238684f
Flavio Gezzi <gezzi.1958690@studenti.uniroma1.it>
2023-05-13 14:18:36 +0200

    add resgiter form validatore

--- src/formValidation.js
+++ src/formValidation.js
@@ -24,3 +24,52 @@ $(function(){
         }
     })
 })
+
+$(function(){
+    $("#registerForm").validate({
+        rules:{
+            usernamesignup:{
+                required:true,
+            },
+            passwordsignup:{
+                required:true,
+                rangelength: [8, 20]
+            },
+            confirmpasswordsignup:{
+                required:true,
+                equalTo: "#passwordsignup"
+            },
+            emailsignup:{
+                required:true,
+                email:true
+            },
+            namesignup:{
+                required:true,
+            },
+            surnamesignup:{
+                required:true,
+            },
+        },
+        messages:{
+            username:{
+                required:"Inserisci il tuo username",
+
+            },
+            passwordsignup:{
+                required: "Inserisci la tua password",
+                rangelength: "La password deve essere lunga minimo 8 e massimo 20 caratteri"
+            },
+            confirmpasswordsignup:{
+                required: "Inserisci la tua password",
+                equalTo: "Le due password non coincidono"
+            },
+            emailsignup:{
+                required: "Inserisci la tua email",
+                email: "Inserisci una email valida"
+            }
+        },
+        submitHandler:function(form){
+            form.submit();
+        }
+    })
+})
